fd44c77a8e070937bb90c50b6277421fbe971875
CFG <carlosfgutier@gmail.com>
2020-04-28 17:18:17 -0500

    added commend 2

--- logic.js
+++ logic.js
@@ -1,1 +1,3 @@
 //ALERT "I know this isn't pretty, but that HTML though. *This is me inviting you to inspect this page* :)"
+
+//Add command f like function that lets them search for words and provides a score on how much of a match their search is with my resume (i.e., % of search words found)
