22116516d4d9059599b0a35fcf4784168512d7aa
CFG <carlosfgutier@gmail.com>
2020-04-29 09:45:43 -0500

    notes for now

--- logic.js
+++ logic.js
@@ -1,7 +1,5 @@
 //ALERT "I know this isn't pretty, but that HTML though. *This is me inviting you to inspect this page* :)"
 
-//Add command f like function that lets them search for words and provides a score on how much of a match their search is with my resume (i.e., % of search words found)
-
-//Add full list of skills that unfold when user hovers over it
-
 // add option to copy template HTML template
+
+//clean css by making hover and active classes for link instead of using the same css for all
